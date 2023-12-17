e353027106a055ba6dbd16a32b8439283dff1d1e
Kimon Vogt <kvogt@g.harvard.edu>
2023-12-17 13:52:54 -0500

    I have most of the elements of the Hero component displaying

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,13 +3,23 @@ import Button from './Button'
 
 const Hero = () => {
   return (
-    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 
+    <section className="max-container padding-container flex flex-col gap-10 py-10 pb-32 
                         md:gap-28 lg:py-20 xl:flex-row">
-      {/* <div className="hero-map ">
-        <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
-          <Image src="public/HelpTheFellowShortIco.svg" width={50} height={50} alt="logo" />
-        </div>
-      </div> */}
+      {/* <div className="hero-map "> */}
+      <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
+        <Image src="HelpTheFellowShortIco.svg" width={50} height={50} alt="logo" />
+      </div>
+      {/* </div> */} 
+      <h1 className="bold-40 lg:bold-58">
+        Elevate Their Future. We've Got Early Learning Covered!
+      </h1>
+      <p className="regular-16  text-gray-30 xl:max-w-[520px]">
+        Embark on an early learning journey with HelpTheFellow! Our app introduces preschoolers to 
+        in-demand technical skills, giving your child a head start in the dynamic world of technology. 
+        Start their adventure today for a bright future tomorrow!
+      </p>
+
+
       <div className="flex items-center flex-wrap gap-3">
         <div className="flex items-center gap-1">
           {Array(5).fill(1).map((_,index)=>(
@@ -27,7 +37,7 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className="flex gap-5">
+      <div className="flex flex-col w-full gap-3 sm:flex-row">
         <Button type="button" title="Explore App" variant="btn_gray" />
         <Button type="button" title="How we work" variant="btn_white_text" />
       </div>

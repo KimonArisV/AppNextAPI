114f0259a82c6b7400300cb26700da85715ca782
Kimon Vogt <kvogt@g.harvard.edu>
2023-12-17 14:21:41 -0500

    done with the Hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,13 +3,14 @@ import Button from './Button'
 
 const Hero = () => {
   return (
-    <section className="max-container padding-container flex flex-col gap-10 py-10 pb-32 
+    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 
                         md:gap-28 lg:py-20 xl:flex-row">
-      {/* <div className="hero-map "> */}
-      <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
-        <Image src="HelpTheFellowShortIco.svg" width={50} height={50} alt="logo" />
-      </div>
-      {/* </div> */} 
+      
+      <div className="relative z-10 flex flex-1 flex-col xl:w-1/2 gap-10">
+        {/* <Image src="HelpTheFellowShortIco.svg" width={50} height={50} alt="logo"
+        className = " absolute left-[-5px] top-[-30px] w-10  lg:w-[50px]"/> */}
+      
+     
       <h1 className="bold-40 lg:bold-58">
         Elevate Their Future. We've Got Early Learning Covered!
       </h1>
@@ -32,16 +33,16 @@ const Hero = () => {
               />
           ))}
         </div>
-        <div>
           <p className=" regular-12 lg:regular-20 ml-1">Excellent Reviews</p>
-        </div>
+        
       </div>
 
       <div className="flex flex-col w-full gap-3 sm:flex-row">
         <Button type="button" title="Explore App" variant="btn_gray" />
         <Button type="button" title="How we work" variant="btn_white_text" />
       </div>
-
+      </div>
+      <div className="hero-map " />
       <div className="relative flex flex-1 items-start">
         <div className="relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-gray-500 px-7 py-8">
           <div className="flex flex-row items-center justify-between">
@@ -58,7 +59,7 @@ const Hero = () => {
           </div>
         </div>
       </div>
-
+      
 
     </section>
   )

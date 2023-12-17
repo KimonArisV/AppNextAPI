d1a2c16e29a9d6d5c74021b840531d1a7ae25307
Kimon Vogt <kvogt@g.harvard.edu>
2023-12-17 09:45:20 -0500

    adding the contact info, our communities and learn more as well aa log at the botton of home page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,11 +6,53 @@ import {FOOTER_CONTACT_INFO, FOOTER_LINKS, SOCIALS} from '@/constants';
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className='flexCenter mb-24 flex-col'>
 
-      <div className="border bg-gray-20">
+      {/* div for everything expect the rights reserved */}
+      <div className="padding-container max-container flex w-full flex-col gap-14 ">
+        {/* this div gets the saem components as the above */}
+        <div className="flex flex-col items-start justify-center gap-[10%] mb-16 md:flex-row">
+          <Link href="/" className='mb-10' >
+            <Image src="HelpTheFellow2.svg" alt="logo" width={74} height={29} />
+          </Link>
+        
+          <div className="flex flex-wrap gap-10 sm:justify-between md:flex-1">
+            {FOOTER_LINKS.map((columns)=> (
+              <FooterColumn title={columns.title}>
+                <ul className="regular-14 flex flex-col gap-4 text-gray-30">
+                  {columns.links.map((link)=>
+                  <Link href="/" key={link}>
+                    {link}
+                  </Link>
+                  )}
+                </ul>
+              </FooterColumn>
+            ))}
+          </div>
+
+          <div className="flex flex-col gap-5 max-md:mt-5 ">
+            <FooterColumn title={FOOTER_CONTACT_INFO.title}>
+                {FOOTER_CONTACT_INFO.links.map((link)=>
+                  <Link href="/" key={link.label} className='flex gap-4 flex-col'>
+                    <p className='whitespace-nowrap'>
+                      {link.label}
+                    </p>
+                    <p className='medium-14 whitespace-nowrap'>
+                      {link.value}
+                    </p>
+                  </Link>
+                )}
+            </FooterColumn>
+          </div>
+
+          
+        </div>
+      </div>
+
+      <div className="border bg-gray-20 w-full">
         <p className="regular-14 w-full text-center text-gray-50">2024 HelpTheFellow | All rights reserved</p>
       </div>
+
     </footer>
   )
 }

878ce2f3fc679b77f8358b17513307bace9e5734
Kimon Vogt <kvogt@g.harvard.edu>
2023-12-17 11:00:20 -0500

    working on the hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import Image from "next/image";
+import Button from './Button'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 
+                        md:gap-28 lg:py-20 xl:flex-row">
+      <div className="hero-map">
+        <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
+          <Image src="public/HelpTheFellowShortIco.svg" width={50} height={50} alt="logo" />
+        </div>
+      </div>
+
+    </section>
   )
 }
 
